d380961f51273ebd8c04876a2c8d80f044a9004e
twikykw <bafer.two@gmail.com>
2025-11-11 15:06:08 +0300

    Add files via upload

---
+++ RT-sln/Services/NotificationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RustTweaker.Services
+{
+	// Token: 0x0200000E RID: 14
+	public static class NotificationService
+	{
+		// Token: 0x06000065 RID: 101 RVA: 0x00008768 File Offset: 0x00006968
+		public static void ShowNotification(string text, int duration = 1000)
+		{
+			NotificationForm notificationForm = new NotificationForm(text, duration);
+			notificationForm.Show();
+		}
+	}
+}
