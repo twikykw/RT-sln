620fe06076b2434bf5af60838e7722b034602a73
twikykw <bafer.two@gmail.com>
2025-11-11 15:07:29 +0300

    Add files via upload

---
+++ RT-sln/Views/ChoosePath.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace RustTweaker.Views
+{
+	// Token: 0x0200000D RID: 13
+	public partial class ChoosePath : global::System.Windows.Forms.Form
+	{
+		// Token: 0x06000063 RID: 99 RVA: 0x00008138 File Offset: 0x00006338
+		protected override void Dispose(bool disposing)
+		{
+			bool flag = disposing && this.components != null;
+			if (flag)
+			{
+				this.components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		// Token: 0x06000064 RID: 100 RVA: 0x00008170 File Offset: 0x00006370
+		private void InitializeComponent()
+		{
+			this.components = new global::System.ComponentModel.Container();
+			this.BorderlessForm = new global::Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+			this.ChooseFolderLabel = new global::System.Windows.Forms.Label();
+			this.RustPathListBox = new global::System.Windows.Forms.ListBox();
+			this.OKbutton = new global::Guna.UI2.WinForms.Guna2Button();
+			this.CancelBtn = new global::Guna.UI2.WinForms.Guna2Button();
+			base.SuspendLayout();
+			this.BorderlessForm.AnimateWindow = true;
+			this.BorderlessForm.AnimationInterval = 250;
+			this.BorderlessForm.BorderRadius = 15;
+			this.BorderlessForm.ContainerControl = this;
+			this.BorderlessForm.DockIndicatorTransparencyValue = 0.6;
+			this.BorderlessForm.HasFormShadow = false;
+			this.BorderlessForm.ResizeForm = false;
+			this.BorderlessForm.TransparentWhileDrag = true;
+			this.ChooseFolderLabel.AutoSize = true;
+			this.ChooseFolderLabel.Location = new global::System.Drawing.Point(18, 13);
+			this.ChooseFolderLabel.Name = "ChooseFolderLabel";
+			this.ChooseFolderLabel.Size = new global::System.Drawing.Size(522, 21);
+			this.ChooseFolderLabel.TabIndex = 0;
+			this.ChooseFolderLabel.Text = "Обнаружено несколько папок с игрой. Выберите необходимый клиент:";
+			this.RustPathListBox.BackColor = global::System.Drawing.Color.FromArgb(54, 54, 54);
+			this.RustPathListBox.BorderStyle = global::System.Windows.Forms.BorderStyle.None;
+			this.RustPathListBox.ForeColor = global::System.Drawing.Color.White;
+			this.RustPathListBox.ItemHeight = 21;
+			this.RustPathListBox.Location = new global::System.Drawing.Point(18, 52);
+			this.RustPathListBox.Name = "RustPathListBox";
+			this.RustPathListBox.Size = new global::System.Drawing.Size(647, 252);
+			this.RustPathListBox.TabIndex = 1;
+			this.RustPathListBox.TabStop = false;
+			this.OKbutton.BackColor = global::System.Drawing.Color.Transparent;
+			this.OKbutton.BorderColor = global::System.Drawing.Color.Transparent;
+			this.OKbutton.BorderRadius = 3;
+			this.OKbutton.Cursor = global::System.Windows.Forms.Cursors.Hand;
+			this.OKbutton.DisabledState.BorderColor = global::System.Drawing.Color.DarkGray;
+			this.OKbutton.DisabledState.CustomBorderColor = global::System.Drawing.Color.DarkGray;
+			this.OKbutton.DisabledState.FillColor = global::System.Drawing.Color.FromArgb(169, 169, 169);
+			this.OKbutton.DisabledState.ForeColor = global::System.Drawing.Color.FromArgb(141, 141, 141);
+			this.OKbutton.FillColor = global::System.Drawing.Color.FromArgb(106, 106, 106);
+			this.OKbutton.Font = new global::System.Drawing.Font("Segoe UI Semibold", 11f, global::System.Drawing.FontStyle.Bold);
+			this.OKbutton.ForeColor = global::System.Drawing.Color.White;
+			this.OKbutton.HoverState.FillColor = global::System.Drawing.Color.FromArgb(253, 253, 48);
+			this.OKbutton.HoverState.ForeColor = global::System.Drawing.Color.FromArgb(64, 64, 64);
+			this.OKbutton.Location = new global::System.Drawing.Point(197, 320);
+			this.OKbutton.Name = "OKbutton";
+			this.OKbutton.Size = new global::System.Drawing.Size(118, 42);
+			this.OKbutton.TabIndex = 14;
+			this.OKbutton.Text = "OK";
+			this.CancelBtn.BorderColor = global::System.Drawing.Color.Transparent;
+			this.CancelBtn.BorderRadius = 3;
+			this.CancelBtn.Cursor = global::System.Windows.Forms.Cursors.Hand;
+			this.CancelBtn.DisabledState.BorderColor = global::System.Drawing.Color.DarkGray;
+			this.CancelBtn.DisabledState.CustomBorderColor = global::System.Drawing.Color.DarkGray;
+			this.CancelBtn.DisabledState.FillColor = global::System.Drawing.Color.FromArgb(169, 169, 169);
+			this.CancelBtn.DisabledState.ForeColor = global::System.Drawing.Color.FromArgb(141, 141, 141);
+			this.CancelBtn.FillColor = global::System.Drawing.Color.FromArgb(106, 106, 106);
+			this.CancelBtn.Font = new global::System.Drawing.Font("Segoe UI Semibold", 11f, global::System.Drawing.FontStyle.Bold);
+			this.CancelBtn.ForeColor = global::System.Drawing.Color.White;
+			this.CancelBtn.HoverState.FillColor = global::System.Drawing.Color.FromArgb(253, 253, 48);
+			this.CancelBtn.HoverState.ForeColor = global::System.Drawing.Color.FromArgb(64, 64, 64);
+			this.CancelBtn.Location = new global::System.Drawing.Point(367, 320);
+			this.CancelBtn.Name = "CancelBtn";
+			this.CancelBtn.Size = new global::System.Drawing.Size(118, 42);
+			this.CancelBtn.TabIndex = 15;
+			this.CancelBtn.Text = "Отмена";
+			base.AutoScaleDimensions = new global::System.Drawing.SizeF(96f, 96f);
+			base.AutoScaleMode = global::System.Windows.Forms.AutoScaleMode.Dpi;
+			this.BackColor = global::System.Drawing.Color.FromArgb(42, 42, 42);
+			base.ClientSize = new global::System.Drawing.Size(683, 380);
+			base.Controls.Add(this.CancelBtn);
+			base.Controls.Add(this.OKbutton);
+			base.Controls.Add(this.RustPathListBox);
+			base.Controls.Add(this.ChooseFolderLabel);
+			this.Font = new global::System.Drawing.Font("Segoe UI", 12f);
+			this.ForeColor = global::System.Drawing.Color.White;
+			base.FormBorderStyle = global::System.Windows.Forms.FormBorderStyle.None;
+			base.Margin = new global::System.Windows.Forms.Padding(4, 5, 4, 5);
+			base.Name = "ChoosePath";
+			base.ShowIcon = false;
+			base.StartPosition = global::System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ChoosePath";
+			base.ResumeLayout(false);
+			base.PerformLayout();
+		}
+
+		// Token: 0x04000053 RID: 83
+		private global::System.ComponentModel.IContainer components = null;
+
+		// Token: 0x04000054 RID: 84
+		private global::Guna.UI2.WinForms.Guna2BorderlessForm BorderlessForm;
+
+		// Token: 0x04000055 RID: 85
+		private global::System.Windows.Forms.Label ChooseFolderLabel;
+
+		// Token: 0x04000056 RID: 86
+		private global::System.Windows.Forms.ListBox RustPathListBox;
+
+		// Token: 0x04000057 RID: 87
+		private global::Guna.UI2.WinForms.Guna2Button CancelBtn;
+
+		// Token: 0x04000058 RID: 88
+		private global::Guna.UI2.WinForms.Guna2Button OKbutton;
+	}
+}
---
+++ RT-sln/Views/ChoosePath.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using Guna.UI2.WinForms;
+
+namespace RustTweaker.Views
+{
+	// Token: 0x0200000D RID: 13
+	public partial class ChoosePath : Form
+	{
+		// Token: 0x17000009 RID: 9
+		// (get) Token: 0x0600005A RID: 90 RVA: 0x00007CDD File Offset: 0x00005EDD
+		// (set) Token: 0x0600005B RID: 91 RVA: 0x00007CE5 File Offset: 0x00005EE5
+		public string SelectedPath { get; private set; }
+
+		// Token: 0x0600005C RID: 92 RVA: 0x00007CF0 File Offset: 0x00005EF0
+		public ChoosePath(List<string> foundPaths)
+		{
+			this.InitializeComponent();
+			this.SetupListBoxColors();
+			base.AcceptButton = this.OKbutton;
+			base.CancelButton = this.CancelBtn;
+			this.OKbutton.Click += this.OKbutton_Click;
+			this.CancelBtn.Click += this.CancelBtn_Click;
+			this.PopulateListBox(foundPaths);
+			base.FormClosing += this.ChoosePath_FormClosing;
+		}
+
+		// Token: 0x0600005D RID: 93 RVA: 0x00007D80 File Offset: 0x00005F80
+		private void PopulateListBox(List<string> foundPaths)
+		{
+			this.RustPathListBox.Items.Clear();
+			foreach (string item in foundPaths)
+			{
+				this.RustPathListBox.Items.Add(item);
+			}
+			bool flag = this.RustPathListBox.Items.Count > 0;
+			if (flag)
+			{
+				this.RustPathListBox.SelectedIndex = 0;
+			}
+		}
+
+		// Token: 0x0600005E RID: 94 RVA: 0x00007E14 File Offset: 0x00006014
+		private void OKbutton_Click(object sender, EventArgs e)
+		{
+			this.SelectedPath = (this.RustPathListBox.SelectedItem as string);
+			base.DialogResult = DialogResult.OK;
+			base.Close();
+		}
+
+		// Token: 0x0600005F RID: 95 RVA: 0x00007E3D File Offset: 0x0000603D
+		private void CancelBtn_Click(object sender, EventArgs e)
+		{
+			base.DialogResult = DialogResult.Cancel;
+			base.Close();
+		}
+
+		// Token: 0x06000060 RID: 96 RVA: 0x00007E50 File Offset: 0x00006050
+		private void ChoosePath_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			bool flag = base.DialogResult == DialogResult.OK && this.SelectedPath == null;
+			if (flag)
+			{
+				this.SelectedPath = (this.RustPathListBox.SelectedItem as string);
+			}
+		}
+
+		// Token: 0x06000061 RID: 97 RVA: 0x00007E90 File Offset: 0x00006090
+		private void SetupListBoxColors()
+		{
+			this.RustPathListBox.BackColor = Color.FromArgb(45, 45, 48);
+			this.RustPathListBox.ForeColor = Color.White;
+			this.RustPathListBox.DrawMode = DrawMode.OwnerDrawFixed;
+			this.RustPathListBox.DrawItem += this.ListBox1_DrawItem;
+		}
+
+		// Token: 0x06000062 RID: 98 RVA: 0x00007EEC File Offset: 0x000060EC
+		private void ListBox1_DrawItem(object sender, DrawItemEventArgs e)
+		{
+			ListBox listBox = (ListBox)sender;
+			Color color = ((e.State & DrawItemState.Selected) == DrawItemState.Selected) ? Color.FromArgb(45, 45, 48) : listBox.BackColor;
+			Color color2 = ((e.State & DrawItemState.Selected) == DrawItemState.Selected) ? Color.FromArgb(253, 253, 48) : listBox.ForeColor;
+			using (Brush brush = new SolidBrush(color))
+			{
+				using (Brush brush2 = new SolidBrush(color2))
+				{
+					e.Graphics.FillRectangle(brush, e.Bounds);
+					bool flag = e.Index >= 0 && e.Index < listBox.Items.Count;
+					if (flag)
+					{
+						string text = listBox.Items[e.Index].ToString();
+						bool flag2 = (e.State & DrawItemState.Selected) == DrawItemState.Selected;
+						if (flag2)
+						{
+							string text2 = "✓";
+							using (Font font = new Font(e.Font.FontFamily, e.Font.Size, FontStyle.Bold))
+							{
+								SizeF sizeF = e.Graphics.MeasureString(text2, font);
+								float y = (float)e.Bounds.Y + ((float)e.Bounds.Height - sizeF.Height) / 2f;
+								e.Graphics.DrawString(text2, font, brush2, (float)(e.Bounds.X + 2), y);
+							}
+						}
+						SizeF sizeF2 = e.Graphics.MeasureString(text, e.Font);
+						float y2 = (float)e.Bounds.Y + ((float)e.Bounds.Height - sizeF2.Height) / 2f;
+						e.Graphics.DrawString(text, e.Font, brush2, (float)(e.Bounds.X + 20), y2);
+					}
+				}
+			}
+		}
+	}
+}
